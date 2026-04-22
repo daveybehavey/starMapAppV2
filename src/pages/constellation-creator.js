ee4c5c480977cd7576992e9232a3060a239ea81b
daveybehavey <davidiheslop@gmail.com>
2026-04-22 16:09:32 -0700

    [agent] Fill content gaps for 'constellation creator' query

---
+++ src/pages/constellation-creator.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Title, Paragraph, List } from '../components';
+
+const ConstellationCreator = () => {
+    return (
+        <Container>
+            <Title>Become a Starry Illustrator with Our Constellation Creator</Title>
+            <Paragraph>
+                Welcome to the Constellation Creator, your go-to tool for designing
+                stunning star patterns. Unleash your creativity and bring the night sky to life!
+            </Paragraph>
+            <Paragraph>
+                With our user-friendly interface, you can:
+            </Paragraph>
+            <List>
+                <li>Create custom constellations</li>
+                <li>Choose from various celestial themes</li>
+                <li>Share your creations with friends and family</li>
+                <li>Download your art for personal use</li>
+            </List>
+            <Paragraph>
+                Whether you're an amateur astronomer or an art enthusiast, our Constellation Creator
+                is designed for you. Dive into the mysteries of the universe and let your imagination
+                soar across the starlit skies!
+            </Paragraph>
+            <Paragraph>
+                Start creating your own constellations today and make your mark on the cosmos!
+            </Paragraph>
+        </Container>
+    );
